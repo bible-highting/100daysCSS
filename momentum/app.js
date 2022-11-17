56cd6de0f712f1a73de7b07f88c6ae15d9984a25
Bible <bearsk711@gmail.com>
2022-11-17 22:49:25 +0900

    #4.1 Form Submission
    - input의 고유기능 활용하여 빈 정보 제출 방지, 최대 글자수 지정(JS 필요 없음)
    - input 고유기능 활용하려면 form안에 있어야 함

--- momentum/app.js
+++ momentum/app.js
@@ -3,12 +3,7 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 function handleLoginBtnClick() {
-  const username = loginInput.value;
-  if (username === "") {
-    alert("Please Write your name.");
-  } else if (username.length > 15) {
-    alert("Your name is too long.")
-  }
+  
 }
 
 loginButton.addEventListener("click", handleLoginBtnClick);
