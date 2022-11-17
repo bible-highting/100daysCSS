b38fa1bd1c9a8f6b2cfa741c975cf51fba3a4aab
Bible <bearsk711@gmail.com>
2022-11-17 23:21:30 +0900

    #4.3 Event part Two
    - event로 기본기능 막기
    - <a> 요소 활용 링크클릭 방지 구현

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault(); //기본동작 방지, 여기서 기본동작은 새로고침
   const username = loginInput.value;
   console.log(username);
 }
 
+function handleLinkClick (event) {
+  event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
