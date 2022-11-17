7e3cbc42e5544ed2c82b419b8a6935b76f2a669b
Bible <bearsk711@gmail.com>
2022-11-17 23:06:18 +0900

    #4.2 Events
    - event.preventDefault()를 이용한 기본동작 방지
    - submit 새로고침 방지

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,10 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-function handleLoginBtnClick() {
-  
+function onLoginSubmit(event) {
+  event.preventDefault(); //기본동작 방지, 여기서 기본동작은 새로고침
+  const username = loginInput.value;
+  console.log(username);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
