bb213e817913f1f46de4dd1bb54ee09168288e0f
Bible <bearsk711@gmail.com>
2022-11-17 22:20:33 +0900

    reset

--- momentum/app.js
+++
@@ -1,9 +0,0 @@
-const daysOfWeek = ["m", 't', 'w', 'th', 'f', 'sat', 'sun']
-
-//Get Item from Array
-daysOfWeek;
-daysOfWeek[0];
-
-//Add one more day to the array
-daysOfWeek.push("day");
-console.log(daysOfWeek);
