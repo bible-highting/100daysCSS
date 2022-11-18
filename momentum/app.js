e198484471c52022afd10fc5ac1fefd69bc94f69
Bible <bearsk711@gmail.com>
2022-11-18 13:22:29 +0900

    #4.6 Loading Username
    - 중복되는 부분 함수화 리팩토링

--- momentum/app.js
+++ momentum/app.js
@@ -2,16 +2,31 @@ const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"; //string 변수명은 대문자로 설정이 원칙
+const HIDDEN_CLASSNAME = "hidden"; //string 변수명은 대문자로 설정이 원칙, string이 두번이상 반복될 경우 변수화
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
   event.preventDefault(); //기본동작 방지, 여기서 기본동작은 새로고침
   loginForm.classList.add(HIDDEN_CLASSNAME);
+
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) { // 중복되는 부분 함수화 리팩토링
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if ( savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+
 
