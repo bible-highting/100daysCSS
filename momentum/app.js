0d56bb6c74347b6f0f8a38d8a57bf42fd12e7c70
Bible <bearsk711@gmail.com>
2022-11-18 12:51:38 +0900

    #4.5 Saving Username
    - .classList.add(), .classList.remove() 사용해서 요소 display관리
    - .innerText
    -`sting ${변수}`
    - string 변수명은 대문자 설정이 원칙

--- momentum/app.js
+++ momentum/app.js
@@ -1,18 +1,16 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; //string 변수명은 대문자로 설정이 원칙
 
 function onLoginSubmit(event) {
   event.preventDefault(); //기본동작 방지, 여기서 기본동작은 새로고침
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
-}
-
-function handleLinkClick (event) {
-  event.preventDefault();
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
